d3b608174396aac40513c85ef4a5c22af5b76044
Adam Chainz <adam@adamj.eu>
2014-06-04 21:21:05 +0100

    Restore sublime-commands and interpret-with-python plugin

---
+++ python_interpret.py
@@ -0,0 +1,34 @@
+import sublime_plugin
+
+
+class InterpretWithPythonCommand(sublime_plugin.TextCommand):
+    always_import = (
+        'math',
+        'random'
+    )
+
+    def run(self, edit):
+        # Import
+        glob = globals()
+        loc = {}
+        for import_me in self.always_import:
+            module = __import__(import_me, glob, loc, ['*'])
+            for k in dir(module):
+                loc[k] = getattr(module, k)
+
+        # Interpret forwards
+        replaces = []
+        for sel in self.view.sel():
+            if sel.size() > 0:
+                text = self.view.substr(sel)
+
+                try:
+                    evald = str(eval(text, glob, loc))
+                except Exception as e:
+                    evald = str(e)
+                replaces.append((sel, evald))
+
+        for (sel, evald) in reversed(replaces):
+            self.view.replace(edit, sel, evald)
+
+        self.view.end_edit(edit)
