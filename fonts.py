9581424df7ec03e74a311a4c1f17e9bb9acbdbfd
Adam Johnson <me@adamj.eu>
2023-08-02 08:09:12 +0100

    Use 15pt font by default

--- fonts.py
+++ fonts.py
@@ -5,5 +5,5 @@ import sublime_plugin
 class DefaultFontSizeCommand(sublime_plugin.ApplicationCommand):
     def run(self):
         s = sublime.load_settings("Preferences.sublime-settings")
-        s.set("font_size", 16)
+        s.set("font_size", 15)
         sublime.save_settings("Preferences.sublime-settings")
