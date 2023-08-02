67b4e16b245ad704350396afabebbc4c879fd1ca
Adam Johnson <me@adamj.eu>
2023-08-02 08:08:03 +0100

    Remove custom last tab plugin as now built-in

--- last_view.py
+++
@@ -1,8 +0,0 @@
-import sublime_plugin
-
-
-class LastViewCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        active_group = self.window.active_group()
-        view = self.window.views_in_group(active_group)[-1]
-        self.window.focus_view(view)
